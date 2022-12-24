4359c16d6cd3f32a9262270f3956e8d2b1590742
VivekYadavDeveloper <vk662967@gmail.com>
2022-12-24 23:18:05 +0530

    Dark Mode

--- lib/theme_sharepreferences.dart
+++ lib/theme_sharepreferences.dart
@@ -1,21 +1,19 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
 //*** Create a Class for theme
 //*** We Constant KEY
 //*** Create a function to set Theme with Boolean Value with asynchronised method.
 //*** Now Call Share-preferences
 
-class ThemeSharePreferences {
-  static const PREF_KEY = "preferences";
-
-  setTheme(bool value) async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    sharedPreferences.setBool(PREF_KEY, value);
-  }
-
-  //*** Now Create One More function getTheme.
-  getTheme() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.getBool(PREF_KEY) ?? false;
-  }
-}
+// class ThemeSharePreferences {
+//   static const PREF_KEY = "preferences";
+//
+//   setTheme(bool value) async {
+//     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+//     sharedPreferences.setBool(PREF_KEY, value);
+//   }
+//
+//   //*** Now Create One More function getTheme.
+//   getTheme() async {
+//     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+//     return sharedPreferences.getBool(PREF_KEY) ?? false;
+//   }
+// }
